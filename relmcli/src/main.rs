6e3df8dd7e1263adc5697d01af634f8ab36c658f
haggarter <haggarter@gmail.com>
2026-04-27 18:05:38 -0600

    basic clap added for cli

--- relmcli/src/main.rs
+++ relmcli/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Create { path: String },
+    Delete { name: String },
+    Start { name: String },
+    Stop { name: String },
+    Switch { name: String },
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Create { path } => println!("create {}", path),
+        Commands::Delete { name } => println!("delete {}", name),
+        Commands::Start { name } => println!("start {}", name),
+        Commands::Stop { name } => println!("stop {}", name),
+        Commands::Switch { name } => println!("switch {}", name),
+        Commands::List => println!("list"),
+    }
 }
